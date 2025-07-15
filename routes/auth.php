a1f1ca7d027ae2e6d51cee6afe9979732cab85a8
gabrielfpereira <gabrielf.pereira@outlook.com>
2025-07-14 22:31:27 -0300

    recuperação de senha

--- routes/auth.php
+++ routes/auth.php
@@ -10,3 +10,5 @@ Route::get('/logout', function () {
     return redirect()->route('home');
 })->name('logout');
 Route::get('/login', \App\Livewire\Auth\Login::class)->name('login');
+Route::get('/recovery-password', \App\Livewire\Auth\RecoveryPassword::class)->name('auth.recovery-password');
+Route::get('/password/reset', \App\Livewire\Auth\ResetPassword::class)->name('password.reset');
